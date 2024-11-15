792ef95e995825bf388bdaf4f61729a1fd42ab90
Admiral General <muhammadusmanawan88@gmail.com>
2024-11-16 02:32:16 +0500

    Bare-Bone Structure of App

---
+++ 01basicreact/01-basic-react-app/src/Twitter.js
@@ -0,0 +1,10 @@
+function Twitter(){
+    return (   //? same rule only one element can be passed in the return
+        <>   
+            <h1>Our Twitter App</h1>
+            <h2>Our Twitter App</h2>
+        </>
+    );
+}
+
+export default Twitter;
---
+++ 01basicreact/01-basic-vite-app/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube(){
+    return (
+        <>
+        <h1>Youtube App</h1>
+        <h2>Youtube App</h2>
+        </>
+    )
+}
+
+export default Youtube
